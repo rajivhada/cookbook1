6786b495ba2e9165f43eea0096949aba5b6f2f87
root <root@chef-workstation.local>
2016-10-16 05:35:00 +0530

    1st commit

---
+++ nginxchef/README.md
@@ -0,0 +1,4 @@
+# nginxchef
+
+TODO: Enter the cookbook description here.
+
---
+++ nginxchef/metadata.rb
@@ -0,0 +1,15 @@
+name 'nginxchef'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures nginxchef'
+long_description 'Installs/Configures nginxchef'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/nginxchef/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/nginxchef' if respond_to?(:source_url)
